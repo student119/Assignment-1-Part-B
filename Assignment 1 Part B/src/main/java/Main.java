92ec2f41d133c7eb71fa14726ae8c44b873c243c
student119 <37659831+student119@users.noreply.github.com>
2020-11-19 15:26:47 +0000

    Printing Successful

--- Assignment 1 Part B/src/main/java/Main.java
+++ Assignment 1 Part B/src/main/java/Main.java
@@ -1,4 +1,11 @@
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main
 
@@ -8,13 +15,13 @@ public class Main
 
     {
 
-        // ArrayList <Students> students = new ArrayList <Students> (1000);
-        // ArrayList <Modules> modules = new ArrayList <Modules> (100);
-        // ArrayList <Course> courses = new ArrayList <Course> (10);
+        ArrayList <Students> students = new ArrayList <Students> (1000);
+        ArrayList <Modules> modules = new ArrayList <Modules> (100);
+        ArrayList <Course> courses = new ArrayList <Course> (10);
 
-        Students student1;
-        Modules module1;
-        Course course1;
+        // Students student1;
+        // Modules module1;
+        // Course course1;
 
         String studentName = "Craig Walsh";
         int studentAge = 28;
@@ -34,66 +41,32 @@ public class Main
         String moduleCourse = "Computer Science & Information Technology";
         String moduleStudents = "Craig Walsh";
 
-        student1 = new Students (studentName, studentAge, studentDOB, studentID, studentCourse, studentModule);
-        module1 = new Modules (moduleName, moduleID, moduleStudents, moduleCourse);
-        course1 = new Course (courseName, courseModules, courseStudents, courseStartDate, courseEndDate);
+        students.add (new Students (studentName, studentAge, studentDOB, studentID, studentCourse, studentModule));
+        modules.add (new Modules (moduleName, moduleID, moduleStudents, moduleCourse));
+        courses.add (new Course (courseName, courseModules, courseStudents, courseStartDate, courseEndDate));
 
-        System.out.println (student1);
-        System.out.println (module1);
-        System.out.println (course1);
+        DateTimeFormatter format = DateTimeFormat.forPattern ("dd-MM-yy");
+        SimpleDateFormat dt1 = new SimpleDateFormat ("dd-MM-yy");
+
+        for (int i = 0; i < students.size(); i++)
+
+        {
+
+            JOptionPane.showMessageDialog(null, students.get(i).getStudentName() + "\n" + students.get(i).getStudentAge() + "\n"
+                    + students.get(i).getStudentDOB() + "\n" + students.get(i).getStudentID() + "\n" + students.get(i).getStudentUsername() + "\n"
+                    + students.get(i).getStudentCourses() + "\n" + students.get(i).getStudentModules());
+
+            // System.out.println (students.get(i).getStudentName());
+
+//            System.out.println(students.get(i).getStudentName() + "\n" + students.get(i).getStudentAge() + "\n"
+//                    + students.get(i).getStudentDOB() + "\n" + students.get(i).getStudentID() + "\n" + students.get(i).getStudentUsername() + "\n"
+//                    + students.get(i).getStudentCourses() + "\n" + students.get(i).getStudentModules());
+
+        }
 
     }
 
 }
-
-//    List<Course> courses = new ArrayList<Course>();
-//
-//    Module module1 = new Module("Module 1", "CT1");
-//    Module module2 = new Module("Module 2", "CT2");
-//
-//    Student student = new Student("Nathan", 20, new Date(2020, Calendar.FEBRUARY, 10));
-//        student.setId("17731961");
-//
-//                Student student2 = new Student("Student2", 21, new Date(2020, Calendar.FEBRUARY, 11));
-//                student2.setId("12543345");
-//
-//                Student student3 = new Student("Student3", 22, new Date(2020, Calendar.FEBRUARY, 12));
-//                student3.setId("125657544");
-//
-//
-//                module1.addStudent(student);
-//                module1.addStudent(student2);
-//
-//                module2.addStudent(student);
-//                module2.addStudent(student3);
-//
-//                DateTime start = new DateTime(2020, 5, 1, 12, 0);
-//                DateTime end = new DateTime(2020, 12, 1, 12, 0);
-//                Course course1 = new Course("Course 1", start, end);
-//
-//                course1.addModule(module1);
-//                course1.addModule(module2);
-//                courses.add(course1);
-
-//    DateTimeFormatter format = DateTimeFormat.forPattern("dd-MM-yy");
-//    SimpleDateFormat dt1 = new SimpleDateFormat("dd-MM-yy");
-//
-//        for (Course course : courses) {
-//                System.out.println("------------------------------------");
-//                System.out.println("Course name: " + course.getName() + " from " + format.print(course.getStartDate()) + " to " + format.print(course.getEndDate()));
-//
-//                for (Module module : course.getModules()) {
-//        System.out.println("Module: " + module.getId() + "-" + module.getName());
-//        System.out.println("  Students enrolled:");
-//
-//        for (Student students : module.getStudents()) {
-//        System.out.println("    " + students.getName() + " - "  + students.getUsername() + " - " + students.getId() + " - " + dt1.format(students.getDateOfBirth()));
-//        }
-//        }
-//        System.out.println("------------------------------------");
-//        }
-
-//loop over courses and use course.getModules() to get module information after which I can use module.getStudents()
 
 //        ArrayList <String> firstNames = new ArrayList <String> (10);
 //        firstNames.add("Craig");
@@ -122,7 +95,7 @@ public class Main
 //        int randomLowerBound = 1;
 //        int randomUpperBoundName = 10;
 //        int randomUpperBoundID = 999999;
-
+//
 //    testModules.add("CT561 Systems Modelling & Simulation");
 //    testModules.add("CT436 Advanced Professional Skills");
 //    testModules.add("CT4100 Information Retrieval");
@@ -133,7 +106,7 @@ public class Main
 //    testModules.add("CT421 Artificial Intelligence");
 //    testModules.add("CT420 Real Time Systems");
 //    testModules.add("CT414 Distributed Systems & Co-Operative Computing");
-
+//
 //    testStudents.add("Craig Walsh");
 //    testStudents.add("Nathan Ryder");
 //    testStudents.add("David Clifford");
@@ -144,7 +117,7 @@ public class Main
 //    testStudents.add("Orlando Bloom");
 //    testStudents.add("Jaroslaw Janas");
 //    testStudents.add("David Schwimmer");
-
+//
 //        for (int i = 0; i < 5; i++)
 //
 //        {
